fa35c8aebc086274fd5aaecbe4ba5e9ce9bccdcf
Dale Ramirez <dalefirstpage@gmail.com>
2021-05-07 15:12:04 +0800

    Reverted back changes

--- src/base/Gateway.php
+++ src/base/Gateway.php
@@ -196,21 +196,12 @@ abstract class Gateway extends BaseGateway
         
         if ($order) {
             if ($billingAddress = $order->getBillingAddress()) {
-                
-                // Fallback to billing address names if credit card name is empty
-                $firstName = $paymentForm->firstName ?: $billingAddress->firstName;
-                $lastName = $paymentForm->lastName ?: $billingAddress->lastName;
-                
-                // Set top level names to the billing names 
-                $card->setFirstName($firstName);
-                $card->setLastName($lastName);
+                // Set top level names to the billing names
+                $card->setFirstName($billingAddress->firstName);
+                $card->setLastName($billingAddress->lastName);
 
-                // Fallback to credit card form names if billing address name is empty
-                $billingFirstName = $billingAddress->firstName ?: $paymentForm->firstName;
-                $billingLastName = $billingAddress->lastName ?: $paymentForm->lastName;
-                
-                $card->setBillingFirstName($billingFirstName);
-                $card->setBillingLastName($billingLastName);
+                $card->setBillingFirstName($billingAddress->firstName);
+                $card->setBillingLastName($billingAddress->lastName);
                 $card->setBillingAddress1($billingAddress->address1);
                 $card->setBillingAddress2($billingAddress->address2);
                 $card->setBillingCity($billingAddress->city);
@@ -228,13 +219,8 @@ abstract class Gateway extends BaseGateway
             }
 
             if ($shippingAddress = $order->getShippingAddress()) {
-
-                // Fallback to credit card form names if shipping address name is empty
-                $shippingFirstName = $shippingAddress->firstName ?: $paymentForm->firstName;
-                $shippingLastName = $shippingAddress->lastName ?: $paymentForm->lastName;
-                
-                $card->setShippingFirstName($shippingFirstName);
-                $card->setShippingLastName($shippingLastName);
+                $card->setShippingFirstName($shippingAddress->firstName);
+                $card->setShippingLastName($shippingAddress->lastName);
                 $card->setShippingAddress1($shippingAddress->address1);
                 $card->setShippingAddress2($shippingAddress->address2);
                 $card->setShippingCity($shippingAddress->city);
